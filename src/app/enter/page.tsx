a7b941ea74718ee31c28e344e0f5b2e035f739ff
PoonyawatSantiniwat <poonyawat@playtorium.co.th>
2026-01-04 01:17:11 +0700

    dynamic

--- src/app/enter/page.tsx
+++ src/app/enter/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import EnterClient from "./EnterClient";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
