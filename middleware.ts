5faae65a5b47cdd193fc86a6aedeec73051d6141
PoonyawatSantiniwat <poonyawat@playtorium.co.th>
2026-01-04 01:24:33 +0700

    remove middleware

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,4 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
   console.log("🔥 middleware hit:", req.nextUrl.pathname);
-
-  if (!token && req.nextUrl.pathname.startsWith("/kitchen")) {
-    return NextResponse.redirect(new URL("/enter", req.url));
-  }
 }
-
-export const config = {
-  matcher: [],
-};
